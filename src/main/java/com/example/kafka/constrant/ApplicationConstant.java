6896eb611d672d1c6bdc9748507ab55707d17a2b
Hung.NguyenBa <hung.nguyenba@sts.saigontechnology>
2022-09-23 10:59:25 +0700

    fix topic

--- src/main/java/com/example/kafka/constrant/ApplicationConstant.java
+++ src/main/java/com/example/kafka/constrant/ApplicationConstant.java
@@ -4,5 +4,5 @@ public class ApplicationConstant {
     public static final String KAFKA_LOCAL_SERVER_CONFIG = "localhost:9092";
     public static final String GROUP_ID_STRING = "group-id-string";
     public static final String GROUP_ID_JSON = "group-id-json";
-    public static final String TOPIC_NAME = "mytopic";
+    public static final String TOPIC_NAME = "testTopic";
 }
